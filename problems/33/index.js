ce953a1fd181cf54e0c0986a8e132f609d1e351e
Josh <notjoshmiller@gmail.com>
2015-06-29 03:28:57 +0000

    Do more problems, chew more bubblegum

--- problems/33/index.js
+++ problems/33/index.js
@@ -1,6 +1,19 @@
 var fs  = require("fs");
-fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
-    if (line !== "") {
-        var num = line;
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line, index) {
+    if (line !== "" && index) {
+        var num = parseInt(line, 10);
+        var solutions = 0;
+        var min = Math.floor(Math.sqrt(num));
+        for (var x = 0; x <= min; x++){
+            var y = Math.sqrt(num - (x * x));
+            if (Math.round(y) === y) {
+                if ((x * x) === y) {
+                    solutions += 2;
+                } else {
+                    solutions += 1;
+                }
+            }
+        }
+        console.log(solutions/2);
     }
 });
