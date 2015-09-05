2416ff3eb033d308f7d816c090d13f7e566663cc
Josh <notjoshmiller@gmail.com>
2015-09-05 21:49:54 +0000

    Make 74 solution more readable

--- problems/74/index.js
+++ problems/74/index.js
@@ -1,5 +1,12 @@
 var fs  = require("fs");
+
+// Coin values
 var values = [1, 3, 5];
+
+// Sort values descending -- required for this algorithm
+values = values.sort(function (a, b) { return (b - a); });
+
+// Recursive function to sum coin values and find minimum amount needed
 var minimumCoins = function minimumCoins(number, count, sum) {
     // Base condition
     if (sum === number) {
@@ -7,6 +14,7 @@ var minimumCoins = function minimumCoins(number, count, sum) {
     }
     for (var i in values) {
         var val = values[i];
+        // If coin value is too large go down to the next smallest value
         if ((sum + val) > number) {
             continue;
         } else {
@@ -14,9 +22,9 @@ var minimumCoins = function minimumCoins(number, count, sum) {
         }
     }
 }
+
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
     if (line !== "") {
-    values = values.sort(function (a, b) { return (b - a); });
        console.log(minimumCoins(parseInt(line), 0, 0));
     }
 });
